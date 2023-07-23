8bbd0c0b945f295b123bf10f8ef021bd16bc9ab1
Nathan Vasconcelos <nathanvasconcelos87@gmail.com>
2023-07-23 13:29:58 -0300

    adicionado a fução de bucar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { carregarLivros } from "./carregarLivros.js";
 import { mensagemListaVazia } from "./mensagemListaVazia.js";
 
 const dataLivros = JSON.parse(localStorage.getItem("livros")) || [];
-
+//let livrosbuscados = [];
 //verificar se há livros na lista
 if(dataLivros.length === 0){
     mensagemListaVazia();
@@ -17,7 +17,43 @@ dataLivros.forEach(livro => {
 //adicionarLivro variaveis
 const listaDeLivros = document.getElementById('listaDeLivros');
 const botaoAdicionar = document.getElementById('botaoAdicionar');
+const botaoPesquisar = document.getElementById('botaoPesquisar');
+const botaoPesquisarMobile = document.getElementById('botaoPesquisarMobile');
 
 botaoAdicionar.addEventListener("click", () =>{
     adicionarLivro(dataLivros);
 });
+
+//Buscar livros
+botaoPesquisar.addEventListener("click", () =>{
+    const livroBuscado = document.getElementById('inputPesquisar').value;
+    buscar(livroBuscado);
+});
+
+botaoPesquisarMobile.addEventListener("click", () =>{
+    const livroBucado = document.getElementById('inputPesquisarMobile').value
+    buscar(livroBucado);
+});
+
+function buscar(livroBuscado){
+    console.log("buscando o livro");
+    listaDeLivros.innerHTML = "";
+    //const livrosbuscados = dataLivros.find(livro => livro.livroBuscado == livroBuscado);
+    //const livrosbuscados = dataLivros.filter(livro => livro.livroBuscado in livroBuscado);
+    //const resultadoBusca = dataLivros.indexOf(livroBuscado); //vai retornar -1 caso não encontre a palavra
+
+    dataLivros.forEach(livro =>{
+        //
+        let resultadoBusca = livro.nomeLivro.indexOf(livroBuscado);
+        if(resultadoBusca != -1){
+            console.log(`Livro ${livroBuscado} encontrado`);
+            console.log(livro);
+            console.log(resultadoBusca);
+            carregarLivros(livro);
+        } else{
+            console.log(`Livro: ${livroBuscado} não encontrado`);
+            console.log(livro);
+            console.log(resultadoBusca);
+        }
+    });
+}
